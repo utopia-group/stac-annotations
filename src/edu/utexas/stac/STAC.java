edb49dacd86c2dc83671c55e65af6881937641f1
Valentin Wüstholz <wuestholz@gmail.com>
2016-03-21 11:59:33 -0500

    Add overloads for identifying primitive inputs.

--- src/edu/utexas/stac/STAC.java
+++ src/edu/utexas/stac/STAC.java
@@ -2,13 +2,42 @@ package edu.utexas.stac;
 
 public final class STAC {
 
-    public static void makeSecretInput(Object input) {
-    }
+    public static void makeSecretInput(Object input) { }
 
-    public static void makeUserInput(Object input) {
-    }
+    public static void makeSecretInput(byte input) { }
 
-    public static void assume(boolean condition) {
-    }
+    public static void makeSecretInput(short input) { }
+
+    public static void makeSecretInput(int input) { }
+
+    public static void makeSecretInput(long input) { }
+
+    public static void makeSecretInput(float input) { }
+
+    public static void makeSecretInput(double input) { }
+
+    public static void makeSecretInput(boolean input) { }
+
+    public static void makeSecretInput(char input) { }
+
+    public static void makeUserInput(Object input) { }
+
+    public static void makeUserInput(byte input) { }
+
+    public static void makeUserInput(short input) { }
+
+    public static void makeUserInput(int input) { }
+
+    public static void makeUserInput(long input) { }
+
+    public static void makeUserInput(float input) { }
+
+    public static void makeUserInput(double input) { }
+
+    public static void makeUserInput(boolean input) { }
+
+    public static void makeUserInput(char input) { }
+
+    public static void assume(boolean condition) { }
 
 }
